105d8cd28f5e45ca9d1ddf75a30fed5afa0d27db
Daniel <ds@dsinnig.com>
2015-12-09 19:59:08 -0500

    Added new trade type (BOTrade - BreakOutTrade)

---
+++ BOTrade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NQuotes;
+
+namespace biiuse
+{
+    class BOTrade : Trade
+    {
+        public BOTrade(string _strategyLabel, int _lotDigits, string _logFileName, int _rangeBufferInMicroPips, int _emailNotificationLevel, MqlApi mql4) : base(_strategyLabel, false, _lotDigits, _logFileName, _rangeBufferInMicroPips, _emailNotificationLevel, mql4)
+        {
+            
+        }
+    }
+}
